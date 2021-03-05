9c193575fc63e035e54cd1714de26c63e4085549
Aiya <momunbekova90@gmail.com>
2021-03-05 20:55:33 +0400

    box-shadow

---
+++ script.js
@@ -0,0 +1,26 @@
+let blur = document.querySelector('.blur')
+let spread = document.querySelector('.spread')
+let vertical = document.querySelector('.vertical')
+let horizontal = document.querySelector('.horizontal')
+let color = document.querySelector('.color')
+let box = document.querySelector('.box')
+let boxShadowCode = document.querySelector('.box-shadow-code')
+let settingsRanges = document.querySelectorAll('.settings-block input')
+
+
+
+settingsRanges.forEach(el => {
+    el.addEventListener('input', () => {
+        generateShadow()
+    })
+})
+
+function generateShadow() {
+    const property = `${horizontal.value}px ${vertical.value}px ${blur.value}px ${spread.value}px ${color.value}`
+    box.style.boxShadow = property
+    boxShadowCode.textContent = `box-shadow: ${property};`
+}
+
+generateShadow()
+
+
